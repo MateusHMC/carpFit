42deb0107c8e287b8beb8554c2780fb12ebbf784
Alex-Araujo01 <xandinhoraujo@gmail.com>
2025-09-05 20:53:00 -0300

    Adiconado a authenticatio, controllers, middleware e repositorys

--- carpFit/src/domain/carpfit/routers/usuariosRoute.js
+++
@@ -1,9 +0,0 @@
-const express=require("express")
-const router=express.Router();
-
-const usuariosController = require("../controllers/usuariosController");
-
-
-router.get("/:id",usuariosController.calcular)
-
-module.exports=router
